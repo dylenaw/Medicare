8a7a8a44f7093b84978c49218d174027280bc04b
Dylen Andrew Wolff <dylenaw@gmail.com>
2023-09-13 15:53:18 +0530

    Update Startup.cs

--- Medicare/Startup.cs
+++ Medicare/Startup.cs
@@ -11,3 +11,116 @@ namespace Medicare
         }
     }
 }
+Sure, here's an updated version of the HTML code that includes a color selector, a button to process the image, a separate frame to display the result, and a download button to download the modified image as a JPG:
+
+```html
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Change Background Color</title>
+    <style>
+        canvas {
+            display: none;
+        }
+        #resultFrame {
+            display: none;
+            border: 1px solid #ccc;
+            margin-top: 20px;
+        }
+    </style>
+</head>
+<body>
+    <input type="file" id="imageInput" accept=".jpg">
+    <input type="color" id="colorSelector" value="#FF0000">
+    <button onclick="processImage()">Process Image</button>
+    <br>
+    <canvas id="canvas"></canvas>
+
+    <div id="resultFrame">
+        <img id="resultImage">
+        <br>
+        <a id="downloadLink" style="display: none;" download="modified_image.jpg">
+            <button>Download Image</button>
+        </a>
+    </div>
+
+    <script src="script.js"></script>
+</body>
+</html>
+```
+
+And here's the updated JavaScript (`script.js`) to handle the color selection, display the result, and enable image download:
+
+```javascript
+function processImage() {
+    const imageInput = document.getElementById('imageInput');
+    const colorSelector = document.getElementById('colorSelector');
+    const canvas = document.getElementById('canvas');
+    const resultFrame = document.getElementById('resultFrame');
+    const resultImage = document.getElementById('resultImage');
+    const downloadLink = document.getElementById('downloadLink');
+    const context = canvas.getContext('2d');
+
+    const reader = new FileReader();
+    reader.onload = function(event) {
+        const image = new Image();
+        image.onload = function() {
+            canvas.width = image.width;
+            canvas.height = image.height;
+            context.drawImage(image, 0, 0);
+
+            // Get the selected background color
+            const newBackgroundColor = colorSelector.value;
+
+            // Get the image data
+            const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+            const data = imageData.data;
+
+            // Loop through each pixel and change the background color
+            for (let i = 0; i < data.length; i += 4) {
+                if (data[i] !== 255 || data[i + 1] !== 255 || data[i + 2] !== 255) {
+                    const lightness = (data[i] + data[i + 1] + data[i + 2]) / 3;
+
+                    data[i] = lightness;
+                    data[i + 1] = lightness;
+                    data[i + 2] = lightness;
+
+                    const newColor = hexToRgb(newBackgroundColor);
+                    data[i - 4] = newColor.r;
+                    data[i - 3] = newColor.g;
+                    data[i - 2] = newColor.b;
+                }
+            }
+
+            // Put the modified image data back onto the canvas
+            context.putImageData(imageData, 0, 0);
+
+            // Display the result in the frame
+            resultImage.src = canvas.toDataURL('image/jpeg');
+            resultFrame.style.display = 'block';
+
+            // Show download button
+            downloadLink.style.display = 'block';
+        };
+
+        image.src = event.target.result;
+    };
+
+    // Read the selected image file
+    reader.readAsDataURL(imageInput.files[0]);
+}
+
+// Convert hex color to RGB
+function hexToRgb(hex) {
+    hex = hex.replace(/^#/, '');
+    return {
+        r: parseInt(hex.slice(0, 2), 16),
+        g: parseInt(hex.slice(2, 4), 16),
+        b: parseInt(hex.slice(4, 6), 16)
+    };
+}
+```
+
+This code will allow you to select an image, choose a background color, process the image, display the result in a separate frame, and download the modified image as a JPG.
